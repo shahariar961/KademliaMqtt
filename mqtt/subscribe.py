67af742d0c22991170e212c9b5669765acd01a6b
Fahim Shahariar <49879599+shahariar961@users.noreply.github.com>
2023-01-12 13:11:57 +0100

    Compare date

--- mqtt/subscribe.py
+++ mqtt/subscribe.py
@@ -26,7 +26,7 @@ async def run():
     await server.bootstrap([bootstrap_node])
 
     result = await server.get(key)
-    print("Get result:", result)
+    #print("Get result:", result)
     server.stop()
     topic,newDate=result.split()
     return topic,newDate
@@ -37,22 +37,23 @@ topic,newDate=asyncio.run(run())
 
 parseLastDate=parse(lastDate)
 parseNewDate=parse(newDate)
+if parseNewDate>parseLastDate:
+    def onMessage(client,userdata,msg):
+        print(msg.topic + ":" + msg.payload.decode())
 
-def onMessage(client,userdata,msg):
-    print(msg.topic + ":" + msg.payload.decode())
+    client=paho.Client("p2",clean_session=False)
+    client.on_message=onMessage
+    if client.connect("localhost",1883,60) !=0:
+        print("Could not connect")
+        sys.exit(-1)
 
-client=paho.Client("p2",clean_session=False)
-client.on_message=onMessage
-if client.connect("localhost",1883,60) !=0:
-    print("Could not connect")
-    sys.exit(-1)
+    client.subscribe("test")
 
-client.subscribe("test")
-
-try:
-    client.loop_start()
-except:
-    print("Disconnect")
-time.sleep(2)
-client.disconnect()
-
+    try:
+        client.loop_start()
+    except:
+        print("Disconnect")
+    time.sleep(2)
+    client.disconnect()
+else:
+    print("Data not updated")
